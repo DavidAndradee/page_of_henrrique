40ca5451815264ae5f293bfcc39e59f1771159ad
David Andrade <david1999andrade@gmail.com>
2024-03-20 16:37:52 -0300

    [fix] add javaScript e nova pag

---
+++ control.js
@@ -0,0 +1,6 @@
+function obterHorarioSelecionado() {
+    var select = document.getElementById(".hour");
+    var opcaoSelecionada = select.value;
+    console.log("Opção selecionada:", opcaoSelecionada);
+}
+
